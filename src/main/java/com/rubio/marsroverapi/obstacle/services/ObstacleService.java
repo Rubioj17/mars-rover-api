1ee3e2cdb64425042e6af6e92049bf27a5ed050b
Jesus Rubio <jesusantoniorubiot@gmail.com>
2025-06-23 15:05:13 -0400

    Creacion del Service Obstacle

---
+++ src/main/java/com/rubio/marsroverapi/obstacle/services/ObstacleService.java
@@ -0,0 +1,15 @@
+package com.rubio.marsroverapi.obstacle.services;
+
+import com.rubio.marsroverapi.obstacle.dto.ObstacleDto;
+
+import java.util.List;
+
+public interface ObstacleService {
+
+    List<ObstacleDto> findAllObstacles();
+
+    ObstacleDto createObstacle(int posX, int posY);
+
+    String deleteAllObstacles();
+
+}
