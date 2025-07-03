e49fa72feba8ba0136729bfb15c09871ae3bcbe5
Jesus Rubio <jesusantoniorubiot@gmail.com>
2025-07-03 10:12:54 -0400

    modificacion pequeña

--- src/test/java/com/rubio/marsroverapi/rover/utilities/CircularPositionTest.java
+++ src/test/java/com/rubio/marsroverapi/rover/utilities/CircularPositionTest.java
@@ -3,7 +3,6 @@ package com.rubio.marsroverapi.rover.utilities;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CircularPositionTest {
 
     @Test
-    public void testForward() {
+    public void testForward_WhenLimitFinal() {
         //Antes
         int length = 5;
         int currentPos = 4;
@@ -20,9 +19,51 @@ class CircularPositionTest {
         //Entonces
         int expected = 0;
         int result = CircularPosition.forward(currentPos, length);
-        Assertions.assertEquals(expected, result);
 
         //Despues
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testForward_WhenInitialLimit() {
+        //Antes
+        int length = 5;
+        int currentPos = 0;
+
+        //Entonces
+        int expected = 1;
+        int result = CircularPosition.forward(currentPos, length);
+
+        //Despues
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testBackward_WhenLimitFinal() {
+        //Antes
+        int length = 5;
+        int currentPos = 4;
+
+        //Entonces
+        int expected = 3;
+        int result = CircularPosition.backward(currentPos, length);
+
+        //Despues
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testBackward_WhenLimitInitial() {
+        //Antes
+        int length = 5;
+        int currentPos = 0;
+
+        //Entonces
+        int expected = 4;
+        int result = CircularPosition.backward(currentPos, length);
+
+        //Despues
+        Assertions.assertEquals(expected, result);
     }
 
 }
